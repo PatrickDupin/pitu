d9d764cb393b676d8c132f421c98ee76db023500
patrick <sophia1493>
2020-11-26 17:57:41 -0300

    desenvolvimento da aplicação "backend"

---
+++ backend/dist/controllers/links.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const links = [];
+let proxId = 1;
+function generateCode() {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 5; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+function postLink(req, res) {
+    const link = req.body;
+    link.id = proxId++;
+    link.code = generateCode();
+    link.hits = 0;
+    links.push(link);
+    res.status(201).json(link);
+}
+function getLink(req, res) {
+    res.send('getLink');
+}
+function hitLink(req, res) {
+    res.send('hitLink');
+}
+exports.default = {
+    postLink,
+    getLink,
+    hitLink
+};
