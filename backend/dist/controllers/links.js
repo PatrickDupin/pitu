7904748a70aad3645a4b80385178e21636b05b17
patrick <sophia1493>
2020-11-27 14:57:08 -0300

    desenvolvimento da aplicação "backend"

--- backend/dist/controllers/links.js
+++ backend/dist/controllers/links.js
@@ -1,5 +1,18 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const linksRepository_1 = __importDefault(require("../models/linksRepository"));
 const links = [];
 let proxId = 1;
 function generateCode() {
@@ -11,18 +24,42 @@ function generateCode() {
     return text;
 }
 function postLink(req, res) {
-    const link = req.body;
-    link.id = proxId++;
-    link.code = generateCode();
-    link.hits = 0;
-    links.push(link);
-    res.status(201).json(link);
+    return __awaiter(this, void 0, void 0, function* () {
+        const link = req.body;
+        link.code = generateCode();
+        link.hits = 0;
+        const result = yield linksRepository_1.default.add(link);
+        if (!result.id) {
+            return res.sendStatus(400);
+        }
+        else {
+            link.id = result.id;
+        }
+        res.status(201).json(link);
+    });
 }
 function getLink(req, res) {
-    res.send('getLink');
+    /*
+    const code = req.params.code as string;
+    const link = links.find(item => item.code === code);
+    if (!link) {
+        res.sendStatus(404);
+    } else {
+        res.json(link);
+    }
+    */
 }
 function hitLink(req, res) {
-    res.send('hitLink');
+    /*
+    const code = req.params.code as string;
+    const index = links.findIndex(item => item.code === code);
+    if (index === -1) {
+        res.sendStatus(404);
+    } else {
+        links[index].hits!++;
+        res.json(links[index]);
+    }
+    */
 }
 exports.default = {
     postLink,
